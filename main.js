c23e3d8c77434346ee1697ddf2504885af979aba
habberzabib <145040111+habberzabib@users.noreply.github.com>
2023-09-22 09:27:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//SIMPLE ADDITION CALC
+
+//BTN EVENT LISTENER
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  //INPUT
+  let num1 = +document.getElementById("num1-in").value;
+  let num2 = +document.getElementById("num2-in").value;
+  //PROCESS
+  let total = num1 + num2;
+  //OUTPUT
+  document.getElementById("output").innerHTML = total;
+}
